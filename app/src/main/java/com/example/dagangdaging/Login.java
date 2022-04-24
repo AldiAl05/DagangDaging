6d890918c285bf293e32926977a95a0294f7ae1d
AldiAl05 <aldi0617@gmail.com>
2022-04-24 12:41:48 +0700

    Login,Regist,Forget

---
+++ app/src/main/java/com/example/dagangdaging/Login.java
@@ -0,0 +1,30 @@
+package com.example.dagangdaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+    }
+
+    public void onClickGoToDaftar(View view) {
+        Intent daftar = new Intent(Login .this, Register.class );
+        startActivity(daftar);
+    }
+
+    public void onClickGoToLupaPassword(View view) {
+        Intent lupapw = new Intent(Login .this, ForgetPassword.class );
+        startActivity(lupapw);
+    }
+
+    public void onClickbtnLoginMasuk(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/dagangdaging/Register.java
@@ -0,0 +1,22 @@
+package com.example.dagangdaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+    }
+
+    public void onClickRegistBackToLogin(View view) {
+        Intent bckLogin = new Intent(Register.this, Login.class );
+        startActivity(bckLogin);
+    }
+}
