131a39a145a1c00e4ef2424b78c6ebcff017342b
AldiAl05 <aldi0617@gmail.com>
2022-05-09 12:41:08 +0700

    update

--- app/src/main/java/com/example/dagangdaging/AddChart.java
+++ app/src/main/java/com/example/dagangdaging/AddChart.java
@@ -2,7 +2,9 @@ package com.example.dagangdaging;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddChart extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddChart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_chart);
+    }
+
+    public void checkout(View view) {
+        Intent checkout= new Intent(AddChart .this, Checkout.class );
+        startActivity(checkout);
     }
 }
